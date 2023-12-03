ef32350ccf5f29ef3ae10529595fa55be5d9178e
Jarrod Baumann <j@rrod.io>
2023-12-03 17:27:44 -0600

    simplify day2 test data

--- src/day2.rs
+++ src/day2.rs
@@ -134,27 +134,23 @@ mod tests {
     use crate::day2::part1;
     use crate::day2::part2;
 
-    #[test]
-    fn test_part1() {
-        let input: Vec<String> = vec![
+    fn test_data() -> Vec<String> {
+        return vec![
             String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
             String::from("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"),
             String::from("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"),
             String::from("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"),
             String::from("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"),
         ];
-        assert_eq!(part1(&input), 8);
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(&test_data()), 8);
     }
 
     #[test]
     fn test_part2() {
-        let input: Vec<String> = vec![
-            String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
-            String::from("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"),
-            String::from("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"),
-            String::from("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"),
-            String::from("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"),
-        ];
-        assert_eq!(part2(&input), 2286);
+        assert_eq!(part2(&test_data()), 2286);
     }
 }
