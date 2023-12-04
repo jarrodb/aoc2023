8a022be63d047c291dfcda262c5dcaab0147c20a
Jarrod Baumann <j@rrod.io>
2023-12-03 20:21:08 -0600

    save building the schematic

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use clap::{Parser, Subcommand};
 
 mod day1;
 mod day2;
+mod day3;
 
 #[derive(Parser)]
 #[command(about="Advent of Code 2023")]
@@ -15,6 +16,7 @@ struct Cli {
 enum SubCommand {
     Day1,
     Day2,
+    Day3,
 }
 
 fn read_from_stdin() -> Vec<String> {
@@ -40,6 +42,11 @@ fn main() {
             let data = read_from_stdin();
             println!("part1: {}", day2::part1(&data));
             println!("part2: {}", day2::part2(&data));
+        },
+        Some(SubCommand::Day3) => {
+            let data = read_from_stdin();
+            println!("part1: {}", day3::part1(&data));
+            println!("part2: {}", day3::part2(&data));
         },
         _ => {
             println!("no subcommand given");
